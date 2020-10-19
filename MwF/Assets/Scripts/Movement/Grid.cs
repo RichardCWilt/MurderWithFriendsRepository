918391435ff5866031670d90476e34f473c51da1
Tate O'Brochta <taobrochta@gmail.com>
2020-10-19 19:46:56 -0400

    Begin Creating Grid for Movement
    Started to create the grid for movement. Created a testing GameObject and a Grid script.

---
+++ MwF/Assets/Scripts/Movement/Grid.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Grid
+{
+    private int width;
+    private int height;
+    private float cellSize;
+    private int[,] gridArray; // Multi dimensional array that has two directions
+
+    // This is my Grid Constructor
+    public Grid(int width, int height, float cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        gridArray = new int[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                CreateWorldText(null, gridArray[x, y].ToString(), GetWorldPosition(x, y), 20, Color.white, TextAnchor.MiddleCenter);
+            }
+        }
+    }
+
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize;
+    }
+
+
+
+    // Create Text in World
+    // This function came from Code Monkey, only using it until I can understand how the grid class works and will manipulate into my own code
+    private TextMesh CreateWorldText(Transform parent, string text, Vector3 localPosition, int fontSize, Color color, TextAnchor textAnchor)
+    {
+        GameObject gameObject = new GameObject("World_Text", typeof(TextMesh));
+        Transform transform = gameObject.transform;
+        transform.SetParent(parent, false);
+        transform.localPosition = localPosition;
+        TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+        textMesh.anchor = textAnchor;
+        //textMesh.alignment = textAlignment;
+        textMesh.text = text;
+        textMesh.fontSize = fontSize;
+        textMesh.color = color;
+        //textMesh.GetComponent<MeshRenderer>().sortingOrder = sortingOrder;
+        return textMesh;
+    }
+}
