29d730acdd558c4d660826c500fd4483125f9bf8
Tate O'Brochta <taobrochta@gmail.com>
2020-10-21 17:00:23 -0400

    Finished Creating the Grid Class
    Finished creating the Grid Class that is the foundation for our movement system.

--- MwF/Assets/Scripts/Movement/Grid.cs
+++ MwF/Assets/Scripts/Movement/Grid.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Notifications.iOS;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,32 +10,80 @@ public class Grid
     private int width;
     private int height;
     private float cellSize;
+    private Vector3 originPosition;
     private int[,] gridArray; // Multi dimensional array that has two directions
+    private TextMesh[,] debugTextArray;
 
     // This is my Grid Constructor
-    public Grid(int width, int height, float cellSize)
+    public Grid(int width, int height, float cellSize, Vector3 originPosition)
     {
         this.width = width;
         this.height = height;
         this.cellSize = cellSize;
+        this.originPosition = originPosition;
 
         gridArray = new int[width, height];
+        debugTextArray = new TextMesh[width, height];
 
         for (int x = 0; x < gridArray.GetLength(0); x++)
         {
             for (int y = 0; y < gridArray.GetLength(1); y++)
             {
-                CreateWorldText(null, gridArray[x, y].ToString(), GetWorldPosition(x, y), 20, Color.white, TextAnchor.MiddleCenter);
+                debugTextArray[x, y] = CreateWorldText(null, gridArray[x, y].ToString(), GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * 0.5f, 5, Color.white, TextAnchor.MiddleCenter); // TODO The text is blurry due to the size, it may be better to use a TextMesh Pro. But not sure if I want the text to stay
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1 , y), Color.white, 100f);
             }
         }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
     }
 
     private Vector3 GetWorldPosition(int x, int y)
     {
-        return new Vector3(x, y) * cellSize;
+        return new Vector3(x, y) * cellSize + originPosition;
     }
 
+    private void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+    }
 
+    public void SetValue(int x, int y, int value)
+    {
+        // Ignoring invalid values
+        // Meaning outside the grid
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            gridArray[x, y] = value;
+            debugTextArray[x, y].text = gridArray[x, y].ToString();
+        }
+    }
+
+    public void SetValue(Vector3 worldPosition, int value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
+    }
+
+    public int GetValue(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            return gridArray[x, y];
+        }else
+        {
+            return 0; // This is an invalid value and have to decide how I want to deal with this
+        }
+    }
+
+    public int GetValue(Vector3 worldPosition)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetValue(x, y);
+    }
 
     // Create Text in World
     // This function came from Code Monkey, only using it until I can understand how the grid class works and will manipulate into my own code
--- MwF/Assets/Scripts/Movement/Testing.cs
+++ MwF/Assets/Scripts/Movement/Testing.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+    private Grid grid;
 
     private void Start()
     {
-        Grid grid = new Grid(4 , 2, 1f);
+        grid = new Grid(4 , 2, 1f, new Vector3(20,0));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            grid.SetValue(worldPosition, 56);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(grid.GetValue(worldPosition));
+        }
     }
 }
