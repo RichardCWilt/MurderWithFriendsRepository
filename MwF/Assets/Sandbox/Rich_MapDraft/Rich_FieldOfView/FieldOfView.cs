0919a1839e55a80e6ad98b422af8eccd354aed64
Richard Wilt <richardcwilt@gmail.com>
2020-10-23 17:27:55 -0400

    Testing work on the FOV system
    I started working through a tutorial to set up field of view for the players. I got the FOV mesh to draw properly but it's not actually stopping when it hits a sprite. I've put a comment on the video I was watching in hopes someone gets back to me.

---
+++ MwF/Assets/Sandbox/Rich_MapDraft/Rich_FieldOfView/FieldOfView.cs
@@ -0,0 +1,62 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+
+    private void Start()
+    {
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        float fov = 90f;
+        Vector3 origin = Vector3.zero;
+        int rayCount = 50;
+        float angle = 0f;
+        float angleIncrease = fov / rayCount;
+        float viewDistance = 10f;
+
+
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i = 0; i <= rayCount; i++)
+        {
+            Vector3 vertex;
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(origin, UtilityScript.GetVectorFromAngle(angle), viewDistance);
+            if (raycastHit2D.collider == null)
+            {
+                //No hit
+                vertex = origin + UtilityScript.GetVectorFromAngle(angle) * viewDistance;
+            } else
+            {
+                //Hit object
+                vertex = raycastHit2D.point;
+            }
+            
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0)
+            {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+            vertexIndex++;
+            angle -= angleIncrease;
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+    }
+}
---
+++ MwF/Assets/Sandbox/Rich_MapDraft/Rich_FieldOfView/UtilityScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UtilityScript : MonoBehaviour
+{
+    public static Vector3 GetVectorFromAngle(float angle)
+    {
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+}
